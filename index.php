0b607b7ba594a0f00bc42e5245bccb24778f65ae
Unknown <a15freed@student.his.se>
2018-03-06 23:46:50 +0100

    test

--- index.php
+++ index.php
@@ -1,20 +1,28 @@
+<meta http-equiv="pragma" content="no-cache" />
 <?php
 	
-	include_once "postgresql/dbconnect.php"; // this will include the file dbconnect.php
-   
-	// create table
-    $sql =<<<EOF
-    drop table if exists TESTTABELL cascade;
-	CREATE TABLE TESTTABELL
-      (ID INT PRIMARY KEY 	NOT NULL,
-      TESTTEXT TEXT 		NOT NULL);
-EOF;
+	// this will include the file dbconnect.php which contains credentials
+	include_once "postgresql/dbconnect.php";
 
-   $ret = pg_query($db, $sql);
-   if(!$ret) {
-      echo pg_last_error($db);
-   } else {
-      echo "The table in the database was created with success!\n";
-   }
-   pg_close($db);
+	
+	tryu {
+        // init the database and create table
+		$initQuery = file_get_contents("postgresql/initdb.sql");
+		
+		print_r($initQuery);
+
+		// check if error occured
+		$ret = pg_query($dbconn, $initQuery);
+		if(!$ret) {
+		  echo pg_last_error($db);
+		} else {
+		  echo "The table in the database was created with success!\n";
+		}
+			
+	} catch (PDOException $e) {
+		$errors++;
+		echo "<span id='failText' />Failed initialization of database because of query (in init_db.sql): </span><br>";
+	}
+	
+	pg_close($dbconn);
 ?>
