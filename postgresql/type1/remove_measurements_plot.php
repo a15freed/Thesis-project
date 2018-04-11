6348eec775924e058a0913bb3adc382a9541f571
Unknown <a15freed@student.his.se>
2018-04-11 22:40:57 +0200

    Added remove file for plot
    and changed the file names

---
+++ postgresql/type1/remove_measurements_plot.php
@@ -0,0 +1,4 @@
+<?php
+unlink(dirname(__FILE__) . "/" . 'measurements_plot.txt');
+echo "Removed file measurements";
+?>
---
+++ postgresql/type1/remove_measurements_total.php
@@ -0,0 +1,4 @@
+<?php
+unlink(dirname(__FILE__) . "/" . 'measurements_total.txt');
+echo "Removed file measurements";
+?>
