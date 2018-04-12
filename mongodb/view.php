a2553684e5634ed6a2aece1670c81a43b689f310
a15freed <a15freed@student.his.se>
2018-04-12 23:02:38 +0200

    Added a view file to see the content of database

--- mongodb/view.php
+++ mongodb/view.php
@@ -1,11 +1,11 @@
 <?php
 include "initdb.php";
-$query = new MongoDB\Driver\Query([]);
 
+$query = new MongoDB\Driver\Query([]);
 $rows = $mng->executeQuery("dbName.collectionName", $query);
 
 foreach ($rows as $row)
     {
-         echo "$row->data - $row->info\n";
+       echo "$row->_id - $row->data - $row->info\n";
     }
  ?>
