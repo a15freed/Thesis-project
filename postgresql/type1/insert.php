cdd48003c912758de487ab482f5f241f199cc61a
Unknown <a15freed@student.his.se>
2018-04-14 17:42:56 +0200

    Removed and changed some code
    removed save file for total execution time.
    changed the FOR to WHILE because the variables did not work.

--- postgresql/type1/insert.php
+++ postgresql/type1/insert.php
@@ -11,79 +11,85 @@
 // this will include the file dbconnect.php which contains credentials
 include "../dbconnect.php";
 
-set_time_limit(3600);
+set_time_limit(7200);
 
 // variables
-$inserts = 1000000;      // inserts to do (customers)
-$measures = 1440;      // Measures per inserts and day
-$idM = 123456;        // random measurements ID
-$lampWatt = 60;       // the device watts
-$wattsHour = 25;      // starting watts value for device
-$time = date('Y-m-d H:i:s');
-$timeArray= [];
+$iterations = 10;							// how many runs
+$inserts = 1000;      				// inserts to do (customers)
+$measures = 1440;     				// measures per inserts
+$fileNr = 0;									// save file counter
+$idM = 123456;        				// random measurements ID
+$deviceWatt = 60;     				// the device watts
+$startWatt = 25;      				// starting watts value for device
+$time = date('Y-m-d H:i:s');	// get time
+$timeArray= [];								// array to push response time
 
-// generate json data
+// start iteration
 $index = 1;
-while($index <= $inserts){
-$timeStart = microtime(true);
-
-	$jsonArray = array(
-
-	'smartMeter' => array(
-
-	      'id' => '1',
-	      'device' => 'Eliond',
-	      'sensorType' => 'Electric',
-	      'createdOn' => '20180205',
-	  	),
-
-	'measurements' => array(),
-	);
-
-	for ($i=0; $i <$measures ; $i++) {
-    $idM++;
-
-    // to simulate the consumption in watt for the device
-    $wattsRand = 0;
-    $wattsRand = $wattsRand + rand(0,8); // device is on between 0-8 hours/day
-    $wattsHour = $wattsHour + ($lampWatt * $wattsRand)/1000;
-    $wattsHour = number_format(($wattsHour), 2);
-    $time = date('Y-m-d H:i:s', strtotime($time.'+1 minute'));
-
-		$Data = array(
-	        'id' => $idM,
-	        'date' => $time,
-	        'kWh' => $wattsHour,
-      	);
-		array_push($jsonArray['measurements'], $Data);
-	}
-	$timeEnd = microtime(true);
-
-	// encode array to string
-	$jsonArrayEncoded = json_encode($jsonArray);
-
-	// insert array to database
-	$sqlQuery = "INSERT INTO json_table (data) VALUES ('$jsonArrayEncoded')";
-	$runQuery = pg_query($dbconn, $sqlQuery);
-
-  // increase counter for while loop
+while($index <= $iterations){
 	$index++;
+	$fileNr++;
 
-	//Measure response time and push to array
-	$timeDiff = $timeEnd - $timeStart;
-	$timeDiff = number_format(($timeDiff), 4);
-	array_push($timeArray, $timeDiff);
-}
+	// generate json data
+	$index2 = 1;
+	while($index2 <= $inserts){
+		$timeStart = microtime(true);
+		$index2++;
+		$jsonArray = array(
+
+		'smartMeter' => array(
+
+		      'id' => '1',
+		      'device' => 'Eliond',
+		      'sensorType' => 'Electric',
+		      'createdOn' => '20180205',
+		  	),
+
+		'measurements' => array(),
+		);
+
+		for ($i=0; $i <$measures ; $i++) {
+	    $idM++;
+
+	    // to simulate the consumption in watt for the device
+	    $wattsRand = 0;
+	    $wattsRand = $wattsRand + rand(0,8); // device is on between 0-8 hours/day
+	    $startWatt = $startWatt + ($deviceWatt * $wattsRand)/1000;
+	    $startWatt = number_format(($startWatt), 2);
+	    $time = date('Y-m-d H:i:s', strtotime($time.'+1 minute'));
+
+			$Data = array(
+		        'id' => $idM,
+		        'date' => $time,
+		        'kWh' => $startWatt,
+	      	);
+			array_push($jsonArray['measurements'], $Data);
+		}
+		$timeEnd = microtime(true);
+
+		// encode array to string
+		$jsonArrayEncoded = json_encode($jsonArray);
+
+		// insert array to database
+		$sqlQuery = "INSERT INTO json_table (data) VALUES ('$jsonArrayEncoded')";
+		$runQuery = pg_query($dbconn, $sqlQuery);
+
+		//Measure response time and push to array
+		$timeDiff = $timeEnd - $timeStart;
+		$timeDiff = number_format(($timeDiff), 4);
+		array_push($timeArray, $timeDiff);
+	}
+
 	// write values from timeArray to file
-	$file = 'measurements_plot.txt';
+	$file = 'measurements_plot_'.$fileNr.'.txt';
 	foreach ($timeArray as $key=>$value) {
 		file_put_contents($file, $value.PHP_EOL, FILE_APPEND | LOCK_EX);
-	}
+		}
 
-// calculate execution time
-$exeTime = microtime(true) - $_SERVER["REQUEST_TIME_FLOAT"];
+	//clear timeArray
+	$timeArray= [];
 
-// write values of exeTime to file
-$file = 'measurements_total.txt';
-file_put_contents($file, number_format(($exeTime), 2).PHP_EOL, FILE_APPEND | LOCK_EX);
+	// clear DB after each iteration except after last one
+		include('initdb.php');
+}
 ?>
