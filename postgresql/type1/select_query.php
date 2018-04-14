c62c95ddfa9bc60447aec7c1979a627ec22032dd
Unknown <a15freed@student.his.se>
2018-04-14 20:54:47 +0200

    Removed for loop and used while instead
    also added microtime to measure execution time

--- postgresql/type1/select_query.php
+++ postgresql/type1/select_query.php
@@ -2,23 +2,44 @@
 	// this will include the file dbconnect.php which contains credentials
 	include "../dbconnect.php";
 
+	$iterations = 10;
 	$queries = 1000;
 	$maxRows = 1000;
+	$fileNr = 0;
+	$queryArray= [];
 
-	for ($i=0; $i <=$queries ; $i++) {
+	$index = 1;
+	while($index <= $iterations){
+		$fileNr++;
+		$index++;
+		while ($index2 <= $queries) {
+			$timeStart = microtime(true);
+			$index2++;
 
-		// choose random ID
-		$randomNmb = rand(1,$maxRows);
+			// choose random ID
+			$randomNmb = rand(1,$maxRows);
 
-		// query the database
-		$result = pg_query($dbconn,"SELECT ID FROM json_table WHERE ID=$randomNmb");
+			// query the database
+			try {
+			$result = pg_query($dbconn,"SELECT ID FROM json_table WHERE ID=$randomNmb");	
+			} catch (\Exception $e) {
 
-		// calculate execution time
-		$exeTime = microtime(true) - $_SERVER["REQUEST_TIME_FLOAT"];
+			}
 
-		// write values of exeTime to file
-		$file = 'measurements_query.txt';
-		file_put_contents($file, number_format(($exeTime), 2).PHP_EOL, FILE_APPEND | LOCK_EX);
+			// calculate execution time
+			$timeEnd = microtime(true);
+			$timeDiff = $timeEnd - $timeStart;
+			$timeDiff = number_format(($timeDiff), 6);
+
+			array_push($queryArray, $timeDiff);
+
+		}
+		// write values of timeArray to file
+		$file = 'measurements_query_'.$fileNr.'.txt';
+		foreach ($queryArray as $key=>$value) {
+			file_put_contents($file, $value.PHP_EOL, FILE_APPEND | LOCK_EX);
+			}
 	}
+
 	echo "Select done!";
 ?>
