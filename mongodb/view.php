840878039df86915c67eda6a052404bfd5386446
Unknown <a15freed@student.his.se>
2018-04-16 19:51:25 +0200

    Removed initdb

--- mongodb/view.php
+++ mongodb/view.php
@@ -1,5 +1,5 @@
 <?php
-include "initdb.php";
+$mongo = new MongoDB\Driver\Manager('mongodb://localhost:27017');
 
 $query = new MongoDB\Driver\Query( [] );
 $cursor = $mongo->executeQuery("exjobb.json_smartmeter_data", $query);
