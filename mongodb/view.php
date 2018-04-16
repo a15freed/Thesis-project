ec2e10fad98f8e05683810fe73ede62bcd80a7e3
Unknown <a15freed@student.his.se>
2018-04-16 18:57:12 +0200

    Added view content in db file

--- mongodb/view.php
+++ mongodb/view.php
@@ -1,11 +1,10 @@
 <?php
 include "initdb.php";
 
-$query = new MongoDB\Driver\Query([]);
-$rows = $mng->executeQuery("dbName.collectionName", $query);
+$query = new MongoDB\Driver\Query( [] );
+$cursor = $mongo->executeQuery("exjobb.json_smartmeter_data", $query);
 
-foreach ($rows as $row)
-    {
-       echo "$row->_id - $row->data - $row->info\n";
-    }
+foreach($cursor as $document) {
+    print_r($document);
+}
  ?>
