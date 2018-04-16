f4ed1c90727a7b22ec406c5fea6cd0ae88442eb5
Unknown <a15freed@student.his.se>
2018-04-16 18:55:56 +0200

    Added insert test file

--- mongodb/initdb.php
+++ mongodb/initdb.php
@@ -1,3 +1,4 @@
 <?php
-$mng = new MongoDB\Driver\Manager("mongodb://localhost:27017"); // Driver Object created
-?>
+include "dbconnect.php";
+$mongo->executeCommand('exjobb', new \MongoDB\Driver\Command(["drop" => "json_smartmeter_data"]));
+ ?>
