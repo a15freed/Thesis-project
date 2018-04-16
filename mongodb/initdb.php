5fdb2eaebdb5715f9ea64c27117ab1c3336fc70b
Unknown <a15freed@student.his.se>
2018-04-16 19:51:11 +0200

    Removed dbconnect

--- mongodb/initdb.php
+++ mongodb/initdb.php
@@ -1,4 +1,4 @@
 <?php
-include "dbconnect.php";
+$mongo = new MongoDB\Driver\Manager('mongodb://localhost:27017');
 $mongo->executeCommand('exjobb', new \MongoDB\Driver\Command(["drop" => "json_smartmeter_data"]));
  ?>
