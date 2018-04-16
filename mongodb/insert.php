6643e01bd9d791df3f80d37b946ceea83b4e0303
Unknown <a15freed@student.his.se>
2018-04-16 19:50:52 +0200

    Removed include initdb

--- mongodb/insert.php
+++ mongodb/insert.php
@@ -1,10 +1,9 @@
 <?php
-include "initdb.php";
-$bulk = new MongoDB\Driver\BulkWrite;
-$someData = "Adam";
-$someInfo = "Stenbitsgatan";
-$doc = ["_id" => new MongoDB\BSON\ObjectID, "data" => $someData, "info" => $someInfo];
+$mongo = new MongoDB\Driver\Manager('mongodb://localhost:27017');
 
-$bulk->insert($doc);
-$mng->executeBulkWrite('dbName.collectionName', $bulk);
- ?>
+$bulkWrite = new MongoDB\Driver\BulkWrite;
+$bulkWrite->insert(['name' => 'Ceres', 'size' => 946, 'distance' => 2.766]);
+$bulkWrite->insert(['name' => 'Vesta', 'size' => 525, 'distance' => 2.362]);
+
+$mongo->executeBulkWrite("exjobb.json_smartmeter_data", $bulkWrite);
+?>
