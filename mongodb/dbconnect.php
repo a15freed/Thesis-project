1dc6c2540250cc071a65e8f75491833e1a200813
Unknown <a15freed@student.his.se>
2018-04-16 19:49:33 +0200

    Removed file

--- mongodb/dbconnect.php
+++
@@ -1,8 +0,0 @@
-<?php
-  // connect to mongo
-  $mongo = new MongoDB\Driver\Manager();
-  // select the database
-  $db = $mongo->exjobb;
-  // select the collection
-  $collection = $db->json_smartmeter_data;
-?>
